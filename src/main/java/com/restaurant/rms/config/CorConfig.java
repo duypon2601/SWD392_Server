64e64d6221e36ce4a4938ed76ae3e7f48c0e5036
duypo <duytntse171996@fpt.edu.vn>
2025-02-12 21:20:57 +0700

    add WebMvcConfigurer

--- src/main/java/com/restaurant/rms/config/CorConfig.java
+++ src/main/java/com/restaurant/rms/config/CorConfig.java
@@ -1,5 +1,6 @@
 package com.restaurant.rms.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -22,5 +23,19 @@ public class CorConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Thay * bằng domain frontend nếu có
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
