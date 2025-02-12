7179977f7bc0299db1d040d8691852d7c0f14a83
duypo <duytntse171996@fpt.edu.vn>
2025-02-13 01:46:14 +0700

    add WebMvcConfigurer

--- src/main/java/com/restaurant/rms/controller/FoodController.java
+++ src/main/java/com/restaurant/rms/controller/FoodController.java
@@ -1,12 +1,15 @@
 package com.restaurant.rms.controller;
 
+import com.restaurant.rms.dto.request.FoodDTO;
 import com.restaurant.rms.service.foodService.FoodService;
+import com.restaurant.rms.util.error.IdInvalidException;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -15,11 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 @SecurityRequirement(name = "api")
 public class FoodController {
 
-    @Autowired
-    FoodService foodService;
+    private final FoodService foodService;
 
-//    @PostMapping("/foodcreate")
-//    public ResponseEntity<Food> createFood(@RequestBody FoodDTO foodDTO) {
-//        Food food = foodService.createFood();
-//    }
+    @PostMapping
+    public ResponseEntity<FoodDTO> createFood(@RequestBody FoodDTO foodDTO) throws IdInvalidException {
+        return ResponseEntity.ok(foodService.createFood(foodDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FoodDTO> getFoodById(@PathVariable("id") Integer foodId) throws IdInvalidException {
+        return ResponseEntity.ok(foodService.getFoodById(foodId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FoodDTO>> getAllFood() {
+        return ResponseEntity.ok(foodService.getAllFood());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FoodDTO> updateFood(@RequestBody FoodDTO foodDTO, @PathVariable("id") Integer foodId) throws IdInvalidException {
+        return ResponseEntity.ok(foodService.updateFood(foodDTO, foodId));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFood(@PathVariable("id") Integer foodId) throws IdInvalidException {
+        foodService.deleteFood(foodId);
+        return ResponseEntity.noContent().build();
+    }
 }
