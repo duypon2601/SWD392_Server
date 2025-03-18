e1210d4cdcf292321e2a999be451e6db5eb022ef
duypo <duytntse171996@fpt.edu.vn>
2025-03-18 13:59:24 +0700

    update getDiningTableByRestaurantId

--- src/main/java/com/restaurant/rms/repository/DiningTableRepository.java
+++ src/main/java/com/restaurant/rms/repository/DiningTableRepository.java
@@ -2,8 +2,11 @@ package com.restaurant.rms.repository;
 
 import com.restaurant.rms.entity.DiningTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface DiningTableRepository extends JpaRepository<DiningTable, Integer> {
     Optional<DiningTable> findByQrCode(String qrCode);
+    List<DiningTable> findByRestaurant_RestaurantId(int restaurantId);
 }
