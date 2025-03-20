7ee04b70561251fa4ddd96e5f59c7548207e083e
duypo <duytntse171996@fpt.edu.vn>
2025-03-20 15:45:40 +0700

    update getDiningTableByRestaurantId

--- src/main/java/com/restaurant/rms/entity/Food.java
+++ src/main/java/com/restaurant/rms/entity/Food.java
@@ -4,6 +4,7 @@ import com.restaurant.rms.enums.Status;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.Where;
 
 import java.util.List;
 
@@ -15,26 +16,33 @@ import java.util.List;
 @Entity
 @Table(name = "food")
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Where(clause = "is_deleted = false") // Chỉ lấy các bản ghi chưa bị xóa mềm
 public class Food {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "food_id")
     private int foodId;
-    @Column(name = "name",unique = true)
+
+    @Column(name = "name", unique = true)
     private String name;
+
     @Column(name = "description")
     private String description;
+
     @Column(name = "image_url")
     private String image_url;
+
     @Enumerated(EnumType.STRING)
     Status status;
 
-    //relationship
-    @OneToMany(mappedBy = "food", cascade = CascadeType.ALL, fetch = FetchType.LAZY,orphanRemoval = true)
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false; // Thêm trường isDeleted, mặc định là false
+
+    // Relationship
+    @OneToMany(mappedBy = "food", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     private List<RestaurantMenuItem> menu_item;
 
     @ManyToOne
     @JoinColumn(name = "category_id", referencedColumnName = "category_id")
     private Category category;
-
 }
